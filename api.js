89cfc088eeb574a3a938c31cbde6224394f03c84
reijii <128262831+ireiji@users.noreply.github.com>
2023-08-01 13:59:05 +0800

    Add files via upload

--- api.js
+++ api.js
@@ -1,10 +1,48 @@
-const express = require('express');
-const url = require('url');
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
-const autheduuids = [];
-const authedips = [];
+var autheduuids = [];
+var authedips = []; // Don't worry. we don't log IPs LOL.
 
-const app = express();
+var server = http.createServer(function (req, res) {  
+    if (req.url.includes("/adduuid/")){
+        var q = url.parse(req.url, true).query;
+
+        if (q.uuid != undefined && validateUUID(q.uuid)) {
+            if (!autheduuids.includes(q.uuid)) {
+                if (!authedips.includes(res.connection.remoteAddress)) {
+                    autheduuids.push(q.uuid);
+                    authedips.push(res.connection.remoteAddress);
+
+                    let timer = setTimeout(function() {
+                        autheduuids.shift();
+                        authedips.shift();
+                    }, 14000);
+                    
+                    res.writeHead(200);
+                    res.end('Success!');
+                } else {
+                    res.writeHead(404);
+                    res.end('Forbidden!');
+                }
+            } else {
+                res.writeHead(404);
+                res.end('Forbidden!');
+            }
+        } else {
+            res.writeHead(404);
+            res.end('Forbidden!');
+        }
+    } else if (req.url.includes("/uuidlist")) {
+        res.writeHead(200);
+        autheduuids.filter(Boolean);
+        res.end(autheduuids.join("\n"));
+    } else {
+        res.writeHead(200);
+        res.end('Welcome to our server!\nSit back, relax and enjoy the cup of coffee.');
+    }
+});
 
 function validateUUID(uuid) {
   if (!(isStringUUID(uuid) || isStringUUID(uuid, false)))
@@ -21,41 +59,4 @@ function isStringUUID(value) {
   return UUIDRegex.test(value);
 }
 
-app.get('/', (req, res) => {
-  res.send('Welcome to our server!\nSit back, relax and enjoy a cup of coffee.');
-});
-
-app.get('/uuidlist', (req, res) => {
-  res.send(autheduuids.filter(Boolean).join('\n'));
-});
-
-app.get('/adduuid', (req, res) => {
-  const q = url.parse(req.url, true).query;
-
-  if (q.uuid != undefined && validateUUID(q.uuid)) {
-    if (!autheduuids.includes(q.uuid)) {
-      if (!authedips.includes(req.ip)) {
-        autheduuids.push(q.uuid);
-        authedips.push(req.ip);
-
-        setTimeout(function () {
-          autheduuids.shift();
-          authedips.shift();
-        }, 14000);
-
-        res.status(200).send('Success!');
-      } else {
-        res.status(403).send('Forbidden!');
-      }
-    } else {
-      res.status(403).send('Forbidden!');
-    }
-  } else {
-    res.status(403).send('Forbidden!');
-  }
-});
-
-const port = 10100;
-app.listen(port, () => {
-  console.log(`Server running`);
-});
+server.listen(10100);
